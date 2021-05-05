f481e6d3007904dfc517addb43c66065293952d8
Brenner Haverlock <80720533+theoneking01@users.noreply.github.com>
2021-05-04 21:49:00 -0700

    updated procfile

--- text_snip/config.py
+++ text_snip/config.py
@@ -7,6 +7,9 @@ load_dotenv()
 class Config(object):
     """Set environment variables."""
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.environ('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
